6ab0419d12f469987506559d99f237211a00107c
hellotc <taicheng_huang@sina.cn>
2016-12-25 00:42:59 -0800

    design functions for testing

---
+++ testing/functest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+def add(a, b):
+    return a+b
+
+def substract(a, b):
+    return a-b
+
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    return float(a)/b
+
+
