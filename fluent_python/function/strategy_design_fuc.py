7b7cb7be117f045983c22a5408e80ced45e3005f
helloTC <hitech9310@gmail.com>
2017-08-09 21:13:06 +0800

    two accomplish method for strategy design

---
+++ fluent_python/function/strategy_design_fuc.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from collections import namedtuple
+import inspect
+
+Customer = namedtuple('Customer', 'name fidelity')
+class LineItem(object):
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+    
+    def total(self):
+        return self.price * self.quantity
+
+class Order(object):
+    # the context
+    def __init__(self, customer, cart, promotion=None):
+        self.customer = customer
+        self.cart = cart
+        self.promotion = promotion
+
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sum(item.total() for item in self.cart)
+        return self.__total
+
+    def due(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion(self)
+        return self.total() - discount
+    
+    def __repr__(self):
+        fmt = '<Order total: {:.2f} due: {:.2f}>'
+        return fmt.format(self.total(), self.due())
+
+
+
+def fidelity_promo(order):
+    return order.total() * .05
+
+def bulk_item_promo(order):
+    discount = 0
+    for item in order.cart:
+        if item.quantity >= 20:
+            discount += item.total() * .1
+    return discount
+
+def large_order_promo(order):
+    distinct_items = {item.product for item in order.cart}
+    if len(distinct_items)>=10:
+        return order.total()*.07
+    else:
+        return 0
+
+promos = [fidelity_promo, bulk_item_promo, large_order_promo]
+def best_promo(order):
+    return max(promo(order) for promo in promos)
