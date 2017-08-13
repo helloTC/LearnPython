c654034a0da8b898dd55b03a895bf0dfdc6257bf
hellotc <taicheng_huang@sina.cn>
2017-08-13 06:12:48 -0700

    make vector2d hashable

---
+++ fluent_python/object/vector2d_v2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+class vector2d(object):
+    def __init__(self, x, y):
+        self.__x = float(x)
+        self.__y = float(y)
+    
+    @property
+    def x(self):
+        return self.__x
+    @property
+    def y(self):
+        return self.__y
+
+    def __iter__(self):
+        return (i for i in (self.x, self.y))
