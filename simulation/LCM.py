f8aa21fa1344778c8ab837617f9517c6118f4e00
hellotc <taicheng_huang@sina.cn>
2017-10-12 18:50:12 -0700

    consider situations with the number of customers is 1

--- simulation/LCM.py
+++ simulation/LCM.py
@@ -85,10 +85,14 @@ class compute_avg_time(object):
         avg_queue_time: the average queue time
         """
         queue_time_series = []
-        for i in np.arange(1, self._custom_num):
-            queue_time_series.append(np.sum(self._st[:i]) - np.sum(self._iat[1:(i+1)]))
-        queue_time_series[queue_time_series<0] = 0
-        return np.mean(queue_time_series)
+        queue_time_series.append(0)
+        if self._custom_num < 2:
+            return 0
+        else:
+            for i in np.arange(1, self._custom_num):
+                queue_time_series.append(np.sum(self._st[:i]) - np.sum(self._iat[1:(i+1)]))
+            queue_time_series[queue_time_series<0] = 0
+            return np.mean(queue_time_series)
 
     def avg_service_time(self):
         """
@@ -113,22 +117,25 @@ class compute_avg_time(object):
         --------
         avg_sys_time: average system time  
         """
-        if np.sum(self._iat) < np.sum(self._st[:-2]):
-            avg_sys_time = (np.sum(self._st) + self._iat[0])/self._custom_num
+        if self._custom_num == 1:
+            avg_sys_time = self._st[0] + self._iat[0]
         else:
-            avg_sys_time = (np.sum(self._iat) + st[-1])/self._custom_num
-        return avg_sys_time
+            if np.sum(self._iat) < np.sum(self._st[:-2]):
+                avg_sys_time = (np.sum(self._st) + self._iat[0])/self._custom_num
+            else:
+                avg_sys_time = (np.sum(self._iat) + st[-1])/self._custom_num
+            return avg_sys_time
 
 
 if __name__ == '__main__':
 
-    h_count = 50
+    customer_count = 50
 
     x0_iat = 1155192169
     # interarrival time series
     iat_gen = lcm(16807, 0, 2147483647, x0_iat)
     # generate first 50 random numbers and Xn (Xn will contains 51 numbers)
-    iat_rd = [iat_gen.next() for i in range(h_count-1)]
+    iat_rd = [iat_gen.next() for i in range(customer_count-1)]
 
     xn_iat = [j for i, j in iat_rd]
     u0_iat = 1.0*x0_iat/2147483647
@@ -141,7 +148,7 @@ if __name__ == '__main__':
     # serice time series
     st_gen = lcm(16807, 0, 2147483647, 1806794933)
     # generate first 50 random numbers and Xn (Xn will contains 51 numbers) 
-    st_rd = [st_gen.next() for i in range(h_count-1)]
+    st_rd = [st_gen.next() for i in range(customer_count-1)]
     
     xn_st = [j for i, j in st_rd]
     u0_st = 1.0*x0_st/2147483647
@@ -149,10 +156,14 @@ if __name__ == '__main__':
     un_st = [u0_st] + un_st
 
     st = neg_exp_distribute(8, un_st)
-    
-    # the average time
-    cat_cls = compute_avg_time(iat, st)
-    avg_queue_time = cat_cls.avg_queue_time()
-    avg_service_time = cat_cls.avg_service_time()
-    avg_system_time = cat_cls.avg_system_time()
+   
+    avg_queue_time = []
+    avg_service_time = []
+    avg_system_time = []
+    for i in np.arange(0, customer_count):
+        # the average time
+        cat_cls = compute_avg_time(iat[:(i+1)], st[:(i+1)])
+        avg_queue_time.append(cat_cls.avg_queue_time())
+        avg_service_time.append(cat_cls.avg_service_time())
+        avg_system_time.append(cat_cls.avg_system_time())
 
