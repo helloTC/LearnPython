775bee2c30259ff434183758effc3053f252b943
hellotc <taicheng_huang@sina.cn>
2017-11-01 08:42:59 -0700

    add functions to extract seed points and final points get from green/blue players

--- graph_proj/func_light.py
+++ graph_proj/func_light.py
@@ -9,13 +9,14 @@ class light_smallwd(object):
         self._G = G
         self._nodenum = nodenum
 
-    def pointlight(self, strategy = 'degree'):
+    def pointlight(self, strategy = 'degree', option = 'descend'):
         """
         Function for exploring project of light_smallworld
         """
         green_collect = set()
         blue_collect = set()
-        stablept = set()
+        green_stablept = set()
+        blue_stablept = set()
 
         allpt = set(range(self._nodenum))
         restpt = allpt.difference(green_collect.union(blue_collect))
@@ -28,36 +29,37 @@ class light_smallwd(object):
             else:
                 raise Exception('Bad parameters')
 
-            stablept.add(green_sdpt)
+            green_stablept.add(green_sdpt)
             green_collect.add(green_sdpt)
             green_neigh = set(nx.neighbors(self._G, green_sdpt))
             green_collect.update(green_neigh)
 
             inter_collect = green_collect.intersection(blue_collect)
-            inter_collect.difference_update(stablept)
+            inter_collect.difference_update(green_stablept.union(blue_stablept))
             blue_collect.difference_update(inter_collect)
 
             restpt = allpt.difference(green_collect.union(blue_collect))
             if len(restpt) == 0:
                 self.green_collect = green_collect
                 self.blue_collect = blue_collect
-                self.stablept = stablept
+                self.blue_stablept = blue_stablept
+                self.green_stablept = green_stablept
                 break
 
             if strategy == 'degree':
-                blue_sdpt = _extract_with_degree(self._G, restpt)
+                blue_sdpt = _extract_with_degree(self._G, restpt, option = option)
             elif strategy == 'random':
                 blue_sdpt = random.choice(list(restpt))
             else:
                 raise Exception('Bad parameters')
 
             blue_collect.add(blue_sdpt)
-            stablept.add(blue_sdpt)
+            blue_stablept.add(blue_sdpt)
             blue_neigh = set(nx.neighbors(self._G, blue_sdpt))
             blue_collect.update(blue_neigh)
 
             inter_collect = green_collect.intersection(blue_collect)
-            inter_collect.difference_update(stablept)
+            inter_collect.difference_update(green_stablept.union(blue_stablept))
 
             green_collect.difference_update(inter_collect)
 
@@ -65,15 +67,26 @@ class light_smallwd(object):
             
             self.green_collect = green_collect
             self.blue_collect = blue_collect
-            self.stablept = stablept
+            self.green_stablept = green_stablept
+            self.blue_stablept = blue_stablept
 
     def collect_diff(self):
         return len(self.green_collect) - len(self.blue_collect)
 
-def _extract_with_degree(G, restpt):
+    def get_seedpt(self):
+        return self.green_stablept, self.blue_stablept 
+
+    def get_collect(self):
+        return self.green_collect, self.blue_collect
+
+def _extract_with_degree(G, restpt, option = 'descend'):
     """
     """
-    degree_pair = sorted(G.degree().items(), key=operator.itemgetter(1), reverse = True)
+    if option == 'descend':
+        rv = True
+    elif option == 'ascend':
+        rv = False
+    degree_pair = sorted(G.degree().items(), key=operator.itemgetter(1), reverse = rv)
     degree_list = [i[0] for i in degree_pair]
     index_degree_list = [i in list(restpt) for i in degree_list].index(1)
     return degree_list[index_degree_list]
@@ -99,17 +112,24 @@ if __name__ == '__main__':
     # numdif_n = np.array(numdif_n)
 # ---------------------------------------------------------
     # This part is to compute difference in random situation
+    # nodenum = 150
+    # neighk = 110
+    # p = 0.2
+    # n_perm = 5000
+    # numdif = []
+
+    # lscls = light_smallwd(nodenum, neighk, p)
+    # for i in range(n_perm):
+    #     lscls.pointlight('random')
+    #     numdif.append(lscls.collect_diff())
+# ---------------------------------------------------------
     nodenum = 150
-    neighk = 110
+    neighk = 10
     p = 0.2
-    n_perm = 5000
-    numdif = []
-
+    
     lscls = light_smallwd(nodenum, neighk, p)
-    for i in range(n_perm):
-        lscls.pointlight('random')
-        numdif.append(lscls.collect_diff())
-        
-
-
+    lscls.pointlight('degree', option = 'descend')        
+    green_pt, blue_pt = lscls.get_seedpt()
+    green_collect, blue_collect = lscls.get_collect()
+   
 
